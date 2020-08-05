d84e1c8e6b93fc62ecc737fc0173fd0c3bf35f74
“mcurren” <mike@curren.me>
2020-08-05 12:36:49 -0500

    added vuetify, finished prototype

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <h1 class="text-h6">Roll for LASERS & FEELINGS</h1>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-dialog v-model="dialog" scrollable max-width="300px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn 
+            fab
+            small
+            elevation="1"
+            color="light-blue"
+            v-bind="attrs"
+            v-on="on">
+            <v-icon dark>mdi-head-question-outline</v-icon>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title>What the what?</v-card-title>
+          <v-card-text>
+            <p>Want to play the popular one-page RPG <a href="http://www.onesevendesign.com/laserfeelings/" target="_blank">Lasers & Feelings</a> but don't have any dice to roll?</p>
+            <p>This app has got your back!</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="blue darken-1" text @click="dialog = false">Ok, let me roll!</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data: () => ({
+    dialog: false,
+  }),
+};
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,143 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <main>
+    <section class="settings">
+      <v-card
+        flat
+        outlined>
+        <v-card-text>
+          <h3 class="overline mb-6">Character Number</h3>
+          <v-slider
+            class="mb-2"
+            v-model="targetNumber"
+            thumb-label="always"
+            prepend-icon="mdi-heart"
+            append-icon="mdi-brain"
+            min="2"
+            max="5"
+          ></v-slider>
+
+          <h3 class="overline mb-2">Roll</h3>
+          <v-btn-toggle 
+            class="mb-4"
+            v-model="rollType">
+            <v-btn value="lasers">Lasers</v-btn>
+            <v-btn value="feelings">Feelings</v-btn>
+          </v-btn-toggle>
+
+          <div v-show="hasRollType">
+            <h3 class="overline mb-2">Number Of Dice</h3>
+            <v-btn-toggle 
+              v-model="diceNum">
+              <v-btn value="1">1</v-btn>
+              <v-btn value="2">2</v-btn>
+              <v-btn value="3">3</v-btn>
+            </v-btn-toggle>
+          </div>
+        </v-card-text>
+
+        <v-card-actions v-show="hasRollType && hasDiceNum">
+          <v-btn 
+            tile 
+            x-large 
+            block
+            color="primary" 
+            @click="rollDice()">
+            <v-icon dark left>mdi-dice-multiple-outline</v-icon> Roll!
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </section>
+
+    <section class="outcome" v-if="currentRolls.length">
+      <v-card
+        flat
+        outlined
+        class="mb-6">
+        <v-card-text>
+          <p>
+            <span v-for="(dice, index) in currentRolls" :key="index">
+              <v-icon x-large left>mdi-dice-{{ dice.roll }}-outline</v-icon>
+            </span>
+          </p>
+          <p class="outcome headline mt-4" v-html="showOutcome"></p>
+          <p class="laserfeelings headline mt-4" v-if="hasLaserFeelings">And you have <em>Laser Feelings</em> about it!</p>
+        </v-card-text>
+      </v-card>
+    </section>
+  </main>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    name: 'HelloWorld',
+    data: () => ({
+      targetNumber: 3,
+      rollType: null,
+      diceNum: null,
+      currentRolls: [],
+      outcomeText: [
+        "Oh no, it's a <em>total</em> failure!",
+        "It's a... <em>mixed</em> success.",
+        "Huzzah, you did it.",
+        "Holy <em>$#!%</em>, it's a <strong>critical success</strong>!!!",
+      ],
+    }),
+    computed: {
+      hasRollType () {
+        return (this.rollType) ? true : false
+      },
+      hasDiceNum () {
+        return (this.diceNum) ? true : false
+      },
+      showOutcome () {
+        const successes = this.currentRolls.filter(item => item.result === 'Success')
+        return this.outcomeText[successes.length]
+      },
+      hasLaserFeelings () {
+        return (this.currentRolls.filter(item => item.roll === this.targetNumber).length) ? true : false
+      },
+    },
+    methods: {
+      rollDice () {
+        // reset current rolls
+        this.currentRolls = []
+        // do rolls
+        for (let step = 0; step < parseInt(this.diceNum); step++) {
+          // roll 1d6
+          const roll = 1 + Math.floor(Math.random() * 6)
+          // determine roll result
+          let result = null
+          if (roll === this.targetNumber) {
+            result = 'Success'
+          }
+          else if (roll > this.targetNumber) {
+            result = (this.rollType === 'feelings') ? 'Success' : 'Failure'
+          }
+          else if (roll < this.targetNumber) {
+            result = (this.rollType === 'lasers') ? 'Success' : 'Failure'
+          }
+          // add outcome to currentRolls tally
+          this.currentRolls.push({
+            roll: roll,
+            result: result
+          })
+        }
+        // reset form to start over
+        this.rollType = null
+        this.diceNum = null
+        // log the rolls to console
+        console.log(`rolls:`, this.currentRolls)
+      }
+    }
   }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss" scoped>
+  main {
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 1em;
+  }
+  section {
+    margin: 2em 0;
+  }
 </style>
