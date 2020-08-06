31579cc065e8a4a2121b35ab9f3106fd4f397d84
“mcurren” <mike@curren.me>
2020-08-05 23:29:18 -0500

    mor css improvements. ux transitions

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       flat
       dark>
       <div class="d-flex align-center">
-        <h1 class="text-h6">Roll for LASERS & FEELINGS</h1>
+        <h1 class="text-h6"><span class="hide-xs">Roll 4 </span>LASERS & FEELINGS</h1>
       </div>
 
       <v-spacer></v-spacer>
@@ -23,14 +23,15 @@
             <v-icon>mdi-head-question-outline</v-icon>
           </v-btn>
         </template>
-        <v-card>
+        <v-card
+          shaped>
           <v-card-title>What the what?</v-card-title>
           <v-card-text>
             <p>Want to play the popular one-page RPG <a href="http://www.onesevendesign.com/laserfeelings/" target="_blank">Lasers & Feelings</a> but don't have any dice to roll?</p>
             <p>This app has got your back!</p>
           </v-card-text>
           <v-card-actions>
-            <v-btn text @click="dialog = false">Ok, let me roll!</v-btn>
+            <v-btn text color="success" @click="dialog = false">Ok, let me roll!</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -58,7 +59,8 @@ export default {
 
 <style lang="scss">
   .theme--light > .v-application--wrap {
-    background: lighten(#8BC4FE, 21%);
+    background: lighten(#fab864, 28%);
+    background: linear-gradient(0deg, rgba(90,16,120,0.9) 10%, rgba(177,59,191,0.9) 45%, rgba(250,184,100,0.9) 100%);
   }
   .theme--light.v-icon {
     color: rgba(0,8,37,0.74);
@@ -71,13 +73,24 @@ export default {
     border-color: rgba(0,8,37,0.28);
   }
 
-  .results .mixed {
-    color: darken(#fab864, 15%);
+  .results {
+    .mixed {
+      color: darken(#fab864, 15%);
+    }
+    .win {
+      color: #01a8a5;
+    }
+    .failure {
+      color: #d4396f;
+    }
+    em, strong {
+      white-space: nowrap;
+    }
   }
-  .results .win {
-    color: #01a8a5;
-  }
-  .results .failure {
-    color: #d4396f;
+
+  .hide-xs {
+    @media (max-width: 459px) {
+      display: none;
+    }
   }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,79 +1,95 @@
 <template>
   <main>
-    <section class="settings">
-      <v-card
-        flat
-        shaped
-        outlined>
-        <v-card-text>
-          <h3 class="overline mb-6 mt-0">Character Number</h3>
-          <v-slider
-            v-model="targetNumber"
-            thumb-label="always"
-            color="info"
-            track-color="warning"
-            prepend-icon="mdi-heart"
-            append-icon="mdi-brain"
-            min="2"
-            max="5"
-          ></v-slider>
+    <transition name="rollFade">
+      <section class="settings" v-show="!currentRolls.length">
+        <v-card
+          flat
+          shaped
+          outlined>
+          <v-card-text>
+            <h3 class="overline mb-6 mt-0">Character Number</h3>
+            <v-slider
+              v-model="targetNumber"
+              thumb-label="always"
+              color="info"
+              track-color="warning"
+              prepend-icon="mdi-heart"
+              append-icon="mdi-brain"
+              min="2"
+              max="5"
+            ></v-slider>
 
-          <h3 class="overline mb-0">Roll</h3>
-          <v-btn-toggle 
-            background-color="accent"
-            color="primary"
-            dense
-            v-model="rollType">
-            <v-btn value="lasers">Lasers</v-btn>
-            <v-btn value="feelings">Feelings</v-btn>
-          </v-btn-toggle>
-
-          <div v-show="hasRollType">
-            <h3 class="overline mb-0 mt-3">Number Of Dice</h3>
+            <h3 class="overline mb-0">Roll</h3>
             <v-btn-toggle 
               background-color="accent"
               color="primary"
               dense
-              v-model="diceNum">
-              <v-btn value="1">1</v-btn>
-              <v-btn value="2">2</v-btn>
-              <v-btn value="3">3</v-btn>
+              v-model="rollType">
+              <v-btn value="lasers">Lasers</v-btn>
+              <v-btn value="feelings">Feelings</v-btn>
             </v-btn-toggle>
-          </div>
-        </v-card-text>
 
-        <v-card-actions v-show="hasRollType && hasDiceNum">
+            <div v-show="hasRollType">
+              <h3 class="overline mb-0 mt-3">Number Of Dice</h3>
+              <v-btn-toggle 
+                background-color="accent"
+                color="primary"
+                dense
+                v-model="diceNum">
+                <v-btn value="1">1</v-btn>
+                <v-btn value="2">2</v-btn>
+                <v-btn value="3">3</v-btn>
+              </v-btn-toggle>
+            </div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn
+              v-show="hasRollType && hasDiceNum"
+              class="mb-2"
+              x-large 
+              rounded
+              block
+              elevation="0"
+              color="primary"
+              @click="rollDice()">
+              <v-icon left light>mdi-dice-multiple-outline</v-icon> Roll!
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </section>
+    </transition>
+
+    <transition name="resultFade">
+      <section class="results" v-show="currentRolls.length">
+        <v-card
+          flat
+          shaped
+          outlined
+          class="mb-6">
+          <v-card-text>
+            <p class="rolls mt-3 mb-3">
+              <span v-for="(dice, index) in currentRolls" :key="index">
+                <v-icon x-large>mdi-dice-{{ dice.roll }}-outline</v-icon>
+              </span>
+            </p>
+            <p class="outcome headline mt-3 mb-3" v-html="showOutcome"></p>
+            <p class="laserfeelings headline mt-3 mb-3" v-if="hasLaserFeelings">And you've got <em class="win">Laser Feelings</em>!</p>
+          </v-card-text>
+        </v-card>
+
           <v-btn 
             class="mb-2"
             x-large 
-            rounded
+            shaped
             block
-            elevation="0"
+            elevation="4"
             color="info"
-            @click="rollDice()">
-            <v-icon left light>mdi-dice-multiple-outline</v-icon> Roll!
+            @click="resetCurrentRolls()">
+            New Roll
           </v-btn>
-        </v-card-actions>
-      </v-card>
-    </section>
-
-    <section class="results" v-if="currentRolls.length">
-      <v-card
-        flat
-        shaped
-        outlined
-        class="mb-6">
-        <v-card-text>
-          <p class="rolls mt-3 mb-3">
-            <span v-for="(dice, index) in currentRolls" :key="index">
-              <v-icon x-large left>mdi-dice-{{ dice.roll }}-outline</v-icon>
-            </span>
-          </p>
-          <p class="outcome headline mt-3 mb-3" v-html="showOutcome"></p>
-          <p class="laserfeelings headline mt-3 mb-3" v-if="hasLaserFeelings">And you have <em class="win">Laser Feelings</em> about it!</p>
-        </v-card-text>
-      </v-card>
-    </section>
+      </section>
+    </transition>
   </main>
 </template>
 
@@ -89,7 +105,7 @@
         "Oh no, it's a <em class='failure'>total failure</em>!",
         "It's a... <em class='mixed'>mixed success</em>.",
         "Huzzah, you did it.",
-        "Holy <em>$#!%</em>, it's a <strong class='win'>critical success</strong>!!!",
+        "Holy <em>$#!%</em>, it's a <strong class='win'>critical success</strong>!!",
       ],
     }),
     computed: {
@@ -132,12 +148,15 @@
             result: result
           })
         }
+        // log the rolls to console
+        console.log(`rolls:`, this.currentRolls)
+      },
+      resetCurrentRolls () {
         // reset form to start over
         this.rollType = null
         this.diceNum = null
-        // log the rolls to console
-        console.log(`rolls:`, this.currentRolls)
-      }
+        this.currentRolls = []
+      },
     }
   }
 </script>
@@ -149,5 +168,55 @@
   }
   section {
     margin: 2em 0;
+  }
+
+  .results {
+    .rolls {
+      max-width: 60%;
+      display: flex;
+      justify-content: space-between;
+      @media (min-width: 500px) {
+        max-width: 45%;
+      }
+      .v-icon {
+        font-size: 60px !important;
+      }
+    }
+    .outcome,
+    .laserfeelings {
+      @media (max-width: 600px) {
+        font-size: 1.3rem !important;
+        line-height: 1.8rem;
+      }
+    }
+  }
+
+  .rollFade-leave {
+    transition-delay: 0.6s, 0;
+  }
+  .rollFade-enter-active, 
+  .rollFade-leave-active {
+    transition-property: opacity, transform;
+    transition-duration: 0.6s;
+  }
+  .rollFade-enter, 
+  .rollFade-leave-to {
+    opacity: 0;
+    transform: translateY(-100%);
+  }
+  .resultFade-enter-to {
+    transition-delay: 0.6s;
+  }
+  .resultFade-enter-active, 
+  .resultFade-leave-active {
+    transition-property: opacity, transform;
+    transition-duration: 0.6s;
+  }
+  .resultFade-enter, 
+  .resultFade-leave-to {
+    opacity: 0;
+  }
+  .resultFade-leave-to {
+    transform: translateY(50%);
   }
 </style>
