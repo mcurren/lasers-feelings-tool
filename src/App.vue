81a2d8a7e6d60b76724bdd4603a8f1ab5bfd84e7
“mcurren” <mike@curren.me>
2020-08-06 16:53:37 -0500

    improved about dialogue text

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,12 @@
       flat
       dark>
       <div class="d-flex align-center">
-        <h1 class="text-h6"><span class="hide-xs">Roll 4 </span>LASERS & FEELINGS</h1>
+        <h1 class="text-h6"><span class="hide-xs">Roll For</span> LASERS & FEELINGS</h1>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-dialog v-model="dialog" scrollable max-width="300px">
+      <v-dialog v-model="dialog" scrollable fullscreen max-width="300px">
         <template v-slot:activator="{ on, attrs }">
           <v-btn 
             fab
@@ -23,15 +23,39 @@
             <v-icon>mdi-head-question-outline</v-icon>
           </v-btn>
         </template>
-        <v-card
-          shaped>
+        <v-card>
           <v-card-title>What the what?</v-card-title>
+          <v-divider></v-divider>
           <v-card-text>
-            <p>Want to play the popular one-page RPG <a href="http://www.onesevendesign.com/laserfeelings/" target="_blank">Lasers & Feelings</a> but don't have any dice to roll?</p>
-            <p>This app has got your back!</p>
+            <p style="margin-top:1rem;">Want to play the popular one-page RPG <strong><a href="http://www.onesevendesign.com/laserfeelings/" target="_blank">Lasers & Feelings</a></strong> but don't have any dice to roll? This app has got your back!</p>
+            <p>If you have no idea what Lasers & Feelings is check out the above link to get in the know!</p>
+            <p>Having trouble using this app? See below:</p>
+            <v-expansion-panels
+              flat
+              tile>
+              <v-expansion-panel>
+                <v-expansion-panel-header>The Roll</v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <p><strong>Step 1</strong> - Set your character's Lasers/Feelings number by dragging the slider or clicking on the <span style="white-space: nowrap;">feelings (<v-icon small color="error">mdi-heart</v-icon>)</span> or <span style="white-space: nowrap;">lasers (<v-icon small color="success">mdi-brain</v-icon>)</span> buttons on either side of the slider. The app should remember your set number for future rolls as long as you don't reload the page.</p>
+                  <p><strong>Step 2</strong> - Select if you're using Lasers or Feelings for the action you are rolling for.</p>
+                  <p><strong>Step 3</strong> - Select the number of dice you are rolling for this action. This is probably determined by a discussion with the GM based on the situation.</p>
+                  <p><strong>Step 4</strong> - Click the <em>ROLL</em> button to see the outcome of your rolls.</p>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+              <v-expansion-panel>
+                <v-expansion-panel-header>The Outcome</v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <p>At the top of the outcome you should see dice icons showing the actual rolls you got.<p>
+                  <p>Below the dice icons is some flavor text that explains what the outcome your rolls mean as per the basic rules of the game.</p>
+                  <p>Click the <em>NEW ROLL</em> button when you need to make your next roll in the game.</p>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
           </v-card-text>
+          <v-divider></v-divider>
           <v-card-actions>
-            <v-btn text color="success" @click="dialog = false">Ok, let me roll!</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn text color="primary" @click="dialog = false">Ok, let me roll!</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -71,6 +95,11 @@ export default {
   }
   .theme--light.v-sheet--outlined {
     border-color: rgba(0,8,37,0.28);
+  }
+  .v-expansion-panel-header,
+  .v-expansion-panel-content__wrap {
+    padding-left: 0 !important;
+    padding-right: 0 !important;
   }
 
   .results {
